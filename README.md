5eef382b396a7b31cad243d7a6823e283f765544
Aman Makhija <amanmakhija2205@gmail.com>
2024-02-03 23:34:11 +0530

    adding dynamic background images

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Live Link to the Project
+
+Go to the [live link](https://react-weather-app-aman.vercel.app/) to see the project.
+
 # Getting Started with Create React App
 
 This is a React Weather App using OpenWeatherMap API for weather information
@@ -9,10 +13,15 @@ This is a React Weather App using OpenWeatherMap API for weather information
 Go to the [Open Weather Map Website](https://home.openweathermap.org/api_keys) and create an account.
 After creating the account create an API Key and paste the it in .env file in **REACT_APP_WEATHER_API_KEY** variable.
 
-## Rapid API
+## Rapid API GEODB-CITIES
 
-Go to the [Rapid API Website](https://rapidapi.com/wirefreethought/api/geodb-cities) and create an account.
-After creating the account you can view your API Key in the Code Snippet. Copy the key and paste it in the **REACT_APP_RAPID_API_KEY** variable.
+Go to the [Rapid API GeoDB-Cities Website](https://rapidapi.com/wirefreethought/api/geodb-cities) and create an account.
+After creating the account subscribe to the API by going to the above link and after then you can view your API Key in the Code Snippet. Copy the key and paste it in the **REACT_APP_RAPID_API_KEY** variable.
+
+## RAPID API FREE IMAGES
+
+Go to the [Rapid API Free Images Website](https://rapidapi.com/ruamazi/api/free-images-api) and create an account.
+After creating the account subscribe to the API by going to the above link and after then you can view your API Key in the Code Snippet. Copy the key and paste it in the **REACT_APP_IMAGE_API_KEY** variable.
 
 # Getting Started with the Application
 
@@ -27,3 +36,8 @@ Run **npm install** in the terminal to install all the required dependencies.
 ## Start the server
 
 Run **npm start** in the terminal to start the development server. After starting the server it is accessible on [http://localhost:3000](http://localhost:3000)
+
+# Fun Fact
+
+The application also changes background image based on the city.
+Give it a try [here](https://react-weather-app-aman.vercel.app/) 😉.
--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/currentWeather';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ForeCast from './components/forecast/forecast';
 
 function App() {
-
+  const [imageUrl, setImageUrl] = useState('https://res.cloudinary.com/winterns/image/upload/v1706982390/ckg8yiqyiszpf3zwusjn.jpg');
   const [currentWeather, setcurrentWeather] = useState(null);
   const [forecast, setForeCast] = useState(null);
   const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
   const WEATHER_API_URL = process.env.REACT_APP_WEATHER_API_URL;
 
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${imageUrl})`;
+  }, [imageUrl]);
+
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
+    const city = searchData.label;
+
+    const imageUrl = fetch('https://free-images-api.p.rapidapi.com/images/' + city, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_IMAGE_API_KEY,
+        'X-RapidAPI-Host': process.env.REACT_APP_IMAGE_API_HOST
+      }
+    });
 
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
     const forecastFetch = fetch(`${WEATHER_API_URL}forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
-    Promise.all([currentWeatherFetch, forecastFetch])
+    Promise.all([currentWeatherFetch, forecastFetch, imageUrl])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
         const forecastResponse = await response[1].json();
+        const imageResponse = await response[2].json();
+        // Random number between 0 to 19
+        const random = Math.floor(Math.random() * 20);
 
+        setImageUrl(imageResponse.results[random].image);
         setcurrentWeather({ city: searchData.label, ...weatherResponse });
         setForeCast({ city: searchData.label, ...forecastResponse });
-
       })
       .catch((err) => console.log(err));
   }
